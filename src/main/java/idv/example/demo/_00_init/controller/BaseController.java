735b41ab2c2e87d71130b6824defcf61686d5e04
andystar8530 <andystar8530@gmail.com>
2021-08-11 09:08:55 +0800

    微調 properties

--- src/main/java/idv/example/demo/_00_init/controller/BaseController.java
+++ src/main/java/idv/example/demo/_00_init/controller/BaseController.java
@@ -3,7 +3,6 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
-//@SessionAttributes({"LoginOK"}) 
 public class BaseController {
 	@GetMapping("/")
 	public String index() {
